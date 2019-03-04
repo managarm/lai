f250ce1c42862fb8e718323022db94fa2694624f
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-03-04 18:02:04 +0100

    acpispec: Move ACPI definitions to their own files

--- include/lai/core.h
+++ include/lai/core.h
@@ -1,4 +1,3 @@
-
 /*
  * Lux ACPI Implementation
  * Copyright (C) 2018-2019 by Omar Muhamed
@@ -9,6 +8,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <acpispec/resources.h>
+#include <acpispec/tables.h>
 #include <lai/host.h>
 
 // Even in freestanding environments, GCC requires memcpy(), memmove(), memset()
@@ -30,10 +31,6 @@ int memcmp(const void *, const void *, size_t);
 
 #define ACPI_MAX_NAME            64
 #define ACPI_MAX_RESOURCES        512
-
-#define ACPI_GAS_MMIO            0
-#define ACPI_GAS_IO            1
-#define ACPI_GAS_PCI            2
 
 #define LAI_NAMESPACE_NAME        1
 #define LAI_NAMESPACE_ALIAS        2
@@ -74,153 +71,6 @@ int memcmp(const void *, const void *, size_t);
 #define LAI_STRING_REFERENCE  14
 #define LAI_BUFFER_REFERENCE  15
 #define LAI_PACKAGE_REFERENCE 16
-
-// Device _STA object
-#define ACPI_STA_PRESENT        0x01
-#define ACPI_STA_ENABLED        0x02
-#define ACPI_STA_VISIBLE        0x04
-#define ACPI_STA_FUNCTION        0x08
-#define ACPI_STA_BATTERY        0x10
-
-// FADT Event/Status Fields
-#define ACPI_TIMER            0x0001
-#define ACPI_BUSMASTER            0x0010
-#define ACPI_GLOBAL            0x0020
-#define ACPI_POWER_BUTTON        0x0100
-#define ACPI_SLEEP_BUTTON        0x0200
-#define ACPI_RTC_ALARM            0x0400
-#define ACPI_PCIE_WAKE            0x4000
-#define ACPI_WAKE            0x8000
-
-// FADT Control Block
-#define ACPI_ENABLED            0x0001
-#define ACPI_SLEEP            0x2000
-
-// Parsing Resource Templates
-#define ACPI_RESOURCE_MEMORY        1
-#define ACPI_RESOURCE_IO        2
-#define ACPI_RESOURCE_IRQ        3
-
-// IRQ Flags
-#define ACPI_IRQ_LEVEL            0x00
-#define ACPI_IRQ_EDGE            0x01
-#define ACPI_IRQ_ACTIVE_HIGH        0x00
-#define ACPI_IRQ_ACTIVE_LOW        0x08
-#define ACPI_IRQ_EXCLUSIVE        0x00
-#define ACPI_IRQ_SHARED            0x10
-#define ACPI_IRQ_NO_WAKE        0x00
-#define ACPI_IRQ_WAKE            0x20
-
-typedef struct acpi_rsdp_t
-{
-    char signature[8];
-    uint8_t checksum;
-    char oem[6];
-    uint8_t revision;
-    uint32_t rsdt;
-    uint32_t length;
-    uint64_t xsdt;
-    uint8_t extended_checksum;
-}__attribute__((packed)) acpi_rsdp_t;
-
-typedef struct acpi_header_t
-{
-    char signature[4];
-    uint32_t length;
-    uint8_t revision;
-    uint8_t checksum;
-    char oem[6];
-    char oem_table[8];
-    uint32_t oem_revision;
-    uint32_t creator_id;
-    uint32_t creator_revision;
-}__attribute__((packed)) acpi_header_t;
-
-typedef struct acpi_rsdt_t
-{
-    acpi_header_t header;
-    uint32_t tables[];
-}__attribute__((packed)) acpi_rsdt_t;
-
-typedef struct acpi_gas_t
-{
-    uint8_t address_space;
-    uint8_t bit_width;
-    uint8_t bit_offset;
-    uint8_t access_size;
-    uint64_t base;
-}__attribute__((packed)) acpi_gas_t;
-
-typedef struct acpi_fadt_t
-{
-    acpi_header_t header;
-    uint32_t firmware_control;
-    uint32_t dsdt;        // pointer to dsdt
-
-    uint8_t reserved;
-
-    uint8_t profile;
-    uint16_t sci_irq;
-    uint32_t smi_command_port;
-    uint8_t acpi_enable;
-    uint8_t acpi_disable;
-    uint8_t s4bios_req;
-    uint8_t pstate_control;
-    uint32_t pm1a_event_block;
-    uint32_t pm1b_event_block;
-    uint32_t pm1a_control_block;
-    uint32_t pm1b_control_block;
-    uint32_t pm2_control_block;
-    uint32_t pm_timer_block;
-    uint32_t gpe0_block;
-    uint32_t gpe1_block;
-    uint8_t pm1_event_length;
-    uint8_t pm1_control_length;
-    uint8_t pm2_control_length;
-    uint8_t pm_timer_length;
-    uint8_t gpe0_length;
-    uint8_t gpe1_length;
-    uint8_t gpe1_base;
-    uint8_t cstate_control;
-    uint16_t worst_c2_latency;
-    uint16_t worst_c3_latency;
-    uint16_t flush_size;
-    uint16_t flush_stride;
-    uint8_t duty_offset;
-    uint8_t duty_width;
-
-    // cmos registers
-    uint8_t day_alarm;
-    uint8_t month_alarm;
-    uint8_t century;
-
-    // ACPI 2.0 fields
-    uint16_t boot_flags;
-    uint8_t reserved2;
-    uint32_t flags;
-
-    acpi_gas_t reset_register;
-    uint8_t reset_command;
-    uint8_t reserved3[3];
-
-    uint64_t x_firmware_control;
-    uint64_t x_dsdt;
-
-    acpi_gas_t x_pm1a_event_block;
-    acpi_gas_t x_pm1b_event_block;
-    acpi_gas_t x_pm1a_control_block;
-    acpi_gas_t x_pm1b_control_block;
-    acpi_gas_t x_pm2_control_block;
-    acpi_gas_t x_pm_timer_block;
-    acpi_gas_t x_gpe0_block;
-    acpi_gas_t x_gpe1_block;
-}__attribute__((packed)) acpi_fadt_t;
-
-typedef struct acpi_aml_t        // AML tables, DSDT and SSDT
-{
-    acpi_header_t header;
-    uint8_t data[];
-}__attribute__((packed)) acpi_aml_t;
 
 typedef struct lai_object_t
 {
@@ -347,37 +197,6 @@ inline lai_object_t *lai_arg(lai_state_t *state, int n) {
     return &state->arg[n];
 }
 
-typedef struct acpi_resource_t
-{
-    uint8_t type;
-
-    uint64_t base;            // valid for everything
-
-    uint64_t length;        // valid for I/O and MMIO
-
-    uint8_t address_space;        // these are valid --
-    uint8_t bit_width;        // -- only for --
-    uint8_t bit_offset;        // -- generic registers
-
-    uint8_t irq_flags;        // valid for IRQs
-} acpi_resource_t;
-
-typedef struct acpi_small_irq_t
-{
-    uint8_t id;
-    uint16_t irq_mask;
-    uint8_t config;
-}__attribute__((packed)) acpi_small_irq_t;
-
-typedef struct acpi_large_irq_t
-{
-    uint8_t id;
-    uint16_t size;
-    uint8_t config;
-    uint8_t length;
-    uint32_t irq;
-}__attribute__((packed)) acpi_large_irq_t;
-
 acpi_fadt_t *lai_fadt;
 acpi_aml_t *lai_dsdt;
 size_t lai_ns_size;
@@ -406,3 +225,4 @@ uint16_t lai_read_event();
 void lai_set_event(uint16_t);
 int lai_enter_sleep(uint8_t);
 int lai_pci_route(acpi_resource_t *, uint8_t, uint8_t, uint8_t);
+
