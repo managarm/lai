28d038c642545a45798b935db609272a5b246fed
Thomas Woertman <twoertman@gmail.com>
2019-07-23 19:38:54 +0200

    Implement simple Embedded Controller driver

---
+++ include/lai/drivers/ec.h
@@ -0,0 +1,21 @@
+/*
+ * Lightweight ACPI Implementation
+ * Copyright (C) 2018-2019 the lai authors
+ */
+
+#pragma once
+
+#include <lai/core.h>
+#include <lai/helpers/resource.h>
+
+struct lai_ec_driver {
+    uint16_t cmd_port;
+    uint16_t data_port;
+};
+
+#define LAI_EC_DRIVER_INITIALIZER {0}
+
+void lai_init_ec(lai_nsnode_t *, struct lai_ec_driver *);
+uint8_t lai_read_ec(uint8_t, struct lai_ec_driver *);
+void lai_write_ec(uint8_t, uint8_t, struct lai_ec_driver *);
+uint8_t lai_query_ec(struct lai_ec_driver *);
