341b0c1297297c97b43923c8ac95d2cf0616ae86
Thomas Woertman <twoertman@gmail.com>
2020-01-18 19:04:51 +0100

    Check if ACPI Timer is supported

--- drivers/timer.c
+++ drivers/timer.c
@@ -5,20 +5,28 @@
 
 static acpi_gas_t timer_block;
 static int extended_timer = 0;
+static int supported = 0;
 
-static uint32_t read_timer_value(){
+static uint32_t read_timer_value(){    
     if(timer_block.address_space == ACPI_GAS_IO){
         return laihost_ind(timer_block.base);
-    } else if(timer_block.address_space = ACPI_GAS_MMIO){
+    } else if(timer_block.address_space == ACPI_GAS_MMIO){
         uint32_t *reg = (uint32_t *)(timer_block.base);
         return *reg;
     } else {
         lai_panic("Unknown ACPI Timer address space");
     }
+
+    return 0;
 }
 
-void lai_start_pm_timer(){
+lai_api_error_t lai_start_pm_timer(){
     acpi_fadt_t *fadt = lai_current_instance()->fadt;
+
+    if(fadt->pm_timer_length != 4)
+        return LAI_ERROR_UNSUPPORTED;
+    
+    supported = 1;
 
     if(lai_current_instance()->acpi_revision >= 2 && fadt->x_pm_timer_block.base){
         timer_block = fadt->x_pm_timer_block;
@@ -33,22 +41,33 @@ void lai_start_pm_timer(){
         extended_timer = 1;
 
     lai_set_sci_event(lai_get_sci_event() | ACPI_TIMER);
+
+    return LAI_ERROR_NONE;
 }
 
-void lai_stop_pm_timer(){
+lai_api_error_t lai_stop_pm_timer(){
+    if(!supported)
+        return LAI_ERROR_UNSUPPORTED;
+    
     lai_set_sci_event(lai_get_sci_event() & ~ACPI_TIMER);
+    return LAI_ERROR_NONE;
 }
 
-void lai_busy_wait_pm_timer(uint64_t ms){
+lai_api_error_t lai_busy_wait_pm_timer(uint64_t ms){
+    if(!supported)
+        return LAI_ERROR_UNSUPPORTED;
+    
     // number of ticks per millisecond 3579.545, rounded up to 3580
     uint32_t goal = read_timer_value() + (ms * 3580);
 
     if(!extended_timer && goal > 0xFFFFFF){
         // TODO: Support goal wraparound with 24bit timers
         lai_warn("Timer wraparound is unsupported for 24bit timers, TODO");
-        return;
+        return LAI_ERROR_UNSUPPORTED;
     }
 
     while(read_timer_value() < goal)
         ;
+
+    return LAI_ERROR_NONE;
 }
--- include/lai/drivers/timer.h
+++ include/lai/drivers/timer.h
@@ -11,9 +11,9 @@
 extern "C" {
 #endif
 
-void lai_start_pm_timer();
-void lai_stop_pm_timer();
-void lai_busy_wait_pm_timer(uint64_t);
+lai_api_error_t lai_start_pm_timer();
+lai_api_error_t lai_stop_pm_timer();
+lai_api_error_t lai_busy_wait_pm_timer(uint64_t);
 
 #ifdef __cplusplus
 }
